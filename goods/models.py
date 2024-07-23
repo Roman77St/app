5be7a83c081618eac53fb8f31b0140897709cfa2
Roman <rostr@rambler.ru>
2024-07-23 14:47:46 +0300

    Register simple tag for categories

--- goods/models.py
+++ goods/models.py
@@ -19,9 +19,19 @@ class Products(models.Model):
     discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='скидка в %')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
+
     class Meta:
         db_table = 'product'
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
     def __str__(self) -> str:
         return f'{self.name} Количество - {self.quantity}'
+    
+    def display_id(self):
+        return f'{self.id:05}'
+    
+    def sell_price(self):
+        if self.discount:
+            return round(self.price - self.price * self.discount / 100, 2)
+        return self.price
